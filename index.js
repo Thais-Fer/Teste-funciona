b30a7d9b75521230b97c225207247e48932ac99d
Thais <59621122+Thais-Fer@users.noreply.github.com>
2021-02-20 22:12:23 -0300

    Cadastro

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import InputPandora from "../../components/InputPandora";
+import InputPandoraMedio from "../../components/InputPandoraMedio";
+import LOGO from "../../assets/LOGO.png";
+import { FiMail, FiLock } from "react-icons/fi";
 
-import { ContainerM, ContainerInputM } from "./styles";
+import { Container, CaixaCadastro, CaixaTitulos, Inf, Logo, ImgLogo, ContainerPT2 } from "./styles";
 
-export default function index({ titulo, placeholder, type, icon: Icon }) {
-    const [focus, setFocus] = useState(false);
+export default function index() {
+  return (
+    <Container>
+      {/* <Inf>
+        Devolução grátis em até 25 dias | 40% de desconto em fretes acima de R$
+        150,00 | Parcelamento em até 6x no cartão
+      </Inf> */}
 
-    return (
-        <ContainerM>
-            <div>{titulo}</div>
-            <ContainerInputM focus={focus}>
-                {Icon && <Icon />}
-                <input
-                    onFocus={() => setFocus(true)}
-                    onBlur={() => setFocus(false)}
-                    type={type}
-                    placeholder={placeholder}
-                ></input>
-            </ContainerInputM>
-        </ContainerM>
-    );
+      <Logo>
+        <ImgLogo> <img src={LOGO} alt="pandora" /></ImgLogo>
+      </Logo>
+
+      <CaixaCadastro>
+        <CaixaTitulos>
+          <div>CRIE SUA CONTA PANDORA</div>
+          <span>
+            tenha acesso a promoções exclusivas, fique por dentro das novidades e acompanhe suas compras.
+          </span>
+        </CaixaTitulos>
+        <InputPandora
+          titulo="E-mail"
+          placeholder="digite seu e-mail (email@email.dominio)"
+        />
+
+        <InputPandora
+          titulo="CPF"
+          placeholder="999.999.999-99"
+        />
+        <InputPandora
+          titulo="Nome Completo"
+          placeholder="digite seu nome completo"
+        />
+        <ContainerPT2>
+          <div>
+            <InputPandoraMedio
+              titulo="Telefone celular"
+              placeholder="(xx) xxxxx-xxxx"
+            />
+
+            <InputPandoraMedio
+              titulo="Senha"
+              placeholder="Digite sua senha"
+            />
+
+            <InputPandoraMedio
+              titulo="Confirmar senha"
+              placeholder="Confirme a senha"
+            />
+          </div>
+        </ContainerPT2>
+
+        <button>CADASTRE-SE</button>
+      </CaixaCadastro>
+    </Container>
+  );
 }
