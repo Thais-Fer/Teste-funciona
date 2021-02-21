48bced0a9a0efa48d7c596f17d7ecb1faabc1acb
Thais <59621122+Thais-Fer@users.noreply.github.com>
2021-02-20 22:07:06 -0300

    Input Pandora

--- index.js
+++ index.js
@@ -1,62 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import InputPandora from "../../components/InputPandora";
-import LOGO from "../../assets/LOGO.png";
-import { FiMail, FiLock } from "react-icons/fi";
+import React, { useState } from "react";
 
-import { Container, CaixaLogin, CaixaTitulos, Inf, Logo, TitulosPequenos, ImgLogo } from "./styles";
+import { Container, ContainerInput } from "./styles";
 
-export default function index() {
+export default function index({ titulo, placeholder, type, icon: Icon }) {
+  const [focus, setFocus] = useState(false);
+
   return (
     <Container>
-      {/* <Inf>
-        Devolução grátis em até 25 dias | 40% de desconto em fretes acima de R$
-        150,00 | Parcelamento em até 6x no cartão
-      </Inf> */}
-
-      <Logo>
-
-        <ImgLogo> <img src={LOGO} alt="pandora" /></ImgLogo>
-      </Logo>
-
-      <CaixaLogin>
-        <CaixaTitulos>
-          <div>JÁ TEM UMA CONTA PANDORA?</div>
-          <span>
-            faça seu login agora para acompanhar seus pedido, ter acesso a
-            ofertas exclusicas e muito mais.
-          </span>
-        </CaixaTitulos>
-
-        <InputPandora
-          titulo="E-mail"
-          placeholder="Digite seu login"
-          icon={FiMail}
-        />
-
-        <InputPandora
-          icon={FiLock}
-          titulo="Senha"
-          type="password"
-          required
-          placeholder="Digite sua senha"
-        />
-
-        <a href="forgot">Esqueci minha senha</a>
-        <a href="forgot">não tem senha? cadastre agora</a>
-
-
-        <button>ENTRAR</button>
-
-
-        <TitulosPequenos>
-          <div><span>------------------------------------- É novo por aqui? -------------------------------------</span></div>
-        </TitulosPequenos>
-
-        <Link to="/cadastro">
-          <button>CRIAR CONTA</button>
-        </Link>
-      </CaixaLogin>
+      <div>{titulo}</div>
+      <ContainerInput focus={focus}>
+        {Icon && <Icon />}
+        <input
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+          type={type}
+          placeholder={placeholder}
+        ></input>
+      </ContainerInput>
     </Container>
   );
 }
--- styles.js
+++ styles.js
@@ -1,103 +1,42 @@
-import styled from "styled-components";
-import { shade } from "polished";
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  min-width: 100%;
-  min-height: 100vh;
-`;
-
-export const Inf = styled.div`
-  background: #fedfdd;
+  margin-top: 20px;
   width: 100%;
-  justify-content: center;
-  align-items: center;
-  height: 25px;
-  font-size: 12px;
-  display: flex;
-`;
 
-export const Logo = styled.div`
-  height: 70px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-bottom: 0.2px solid black;
-`;
-export const ImgLogo = styled.div`
- 
-`;
-
-export const CaixaLogin = styled.div`
-  display: flex;
-  flex-direction: column;
-  background: #fff4f2;
-  max-width: 500px;
-  align-items: center;
-  border-radius: 20px;
-  border: 0.5px solid #b38c89;
-  margin-top: 30px;
-  height: 550px;
-  padding-left: 50px;
-  padding-right: 50px;
-  transition: background-color 0.2s;
-
-  button {
-    width: 150px;
-    height: 30px;
-    background: #b38c89;
-    color: white;
-    border-radius: 50px;
-    border: none;
-    margin-top: 20px;
-
-    &:hover {
-      background: ${shade(0.2, "#b38c89")};
-    }
-  }
-
-  a {
-    color: gray;
-    text-decoration: none;
-    font-size: 12px;
-    margin: 5px;
-
-    &:hover {
-      color: ${shade(0.2, "#D66660")};
-    }
-  }
-`;
-
-export const CaixaTitulos = styled.div`
-  line-height: 0.8;
-  text-align: center;
   div {
-    text-align: center;
-    color: #d66660;
-    margin-top: 30px;
-    margin-bottom: 20px;
-    font-size: 18px;
-  }
-
-  span {
-    font-size: 15px;
-    margin-top: 30px;
-    line-height: 1.5;
+    font-size: 12px;
   }
 `;
 
-export const TitulosPequenos = styled.div`
- div{ 
-  text-align: center;
-  justify-content: left;
-  font-size: 12px;
-  margin-top: 40px;
-  color: ${shade(0.2, "#D66660")};
- }
+export const ContainerInput = styled.div`
   
+  margin-bottom: 5px;
+  text-align: left;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  padding-left: 10px;
+  border: 0.5px solid lightgrey;
+  height: 40px;
 
+  ${(props) =>
+    props.focus &&
+    css`
+      border-color: #b38c89;
+      color: #b38c89;
+    `}
+  input {
+    border: 2px solid white;
+    flex: 1;
+    height: 30px;
+    padding-left: 10px;
+  }
+
+  input::placeholder {
+    font-size: 14px;
+    padding-left: 10px;
+  }
 `;
-
